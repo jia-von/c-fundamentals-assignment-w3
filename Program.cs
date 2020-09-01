272c1e341572a7b1667b158f87c24ef39adf7378
Jia <jiavon@ualberta.ca>
2020-09-01 12:29:47 -0600

    c# strings Exercise 4

--- Program.cs
+++ Program.cs
@@ -8,12 +8,10 @@ namespace C_Fundamentals_Assignment_W3
         static void Main(string[] args)
         {
 
-            //Exercise 3: Use the string interpolation method to concatenate two strings:
+            //Exercise 4: Access the first character (H) in myString and print the result:
 
-            string firstName = "John";
-            string lastName = "Doe";
-            string name = $"My full name is: {firstName} {lastName} ";
-            Console.WriteLine(name);
+            string myString = "Hello";
+            Console.WriteLine(myString[0]);
 
         }
     }
