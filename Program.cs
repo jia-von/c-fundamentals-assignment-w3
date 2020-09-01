5e2142251ac20c31d206db236195ea9594beb216
Jia <jiavon@ualberta.ca>
2020-09-01 13:23:36 -0600

    Arrays exercise 5

--- Program.cs
+++ Program.cs
@@ -8,11 +8,12 @@ namespace C_Fundamentals_Assignment_W3
         static void Main(string[] args)
         {
 
-            //Exercise 4: Loop through the elements in the cars array using the foreach loop.
+            //Exercise 5: Use the correct method to sort the cars array.
 
 
 
             string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            Array.Sort(cars);
             foreach (string i in cars)
             {
                 Console.WriteLine(i);
