e477f4247456e74cf2da56333105913fd024d179
Jia <jiavon@ualberta.ca>
2020-09-01 13:53:15 -0600

    Exercise 5, object

--- Program.cs
+++ Program.cs
@@ -5,16 +5,17 @@ namespace C_Fundamentals_Assignment_W3
     class Car
 
     {
-        public void fullThrottle()
+        public string model;
+
+        public Car()
         {
-            Console.WriteLine("The car is going as fast as it can!");
+            model = "Mustang";
         }
         static void Main(string[] args)
         {
-            //Exercise 4: Call the fullThrottle() method from the object:
-            Car myObj = new Car();
-            myObj.fullThrottle();
-
+            //Exercise 5: Create a constructor of Car, and call it:
+            Car Ford = new Car();
+            Console.WriteLine(Ford.model);
         }
 
     }
