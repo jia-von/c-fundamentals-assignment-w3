610af0257be900c6da10bee05249cade01634a3b
Jia <jiavon@ualberta.ca>
2020-09-01 12:26:59 -0600

    c# strings exercise 3

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace C_Fundamentals_Assignment_W3
         static void Main(string[] args)
         {
 
-            //Exercise 2: Use the correct operator to concatenate two strings:
+            //Exercise 3: Use the string interpolation method to concatenate two strings:
 
-            string firstName = "John ";
+            string firstName = "John";
             string lastName = "Doe";
-            string name = firstName + lastName;
+            string name = $"My full name is: {firstName} {lastName} ";
             Console.WriteLine(name);
 
         }
