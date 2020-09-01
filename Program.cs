f7a8a42a9c56968fc157cb6260c34dd998fa0490
Jia <jiavon@ualberta.ca>
2020-09-01 12:45:16 -0600

    IfElseExercise3

--- Program.cs
+++ Program.cs
@@ -8,13 +8,18 @@ namespace C_Fundamentals_Assignment_W3
         static void Main(string[] args)
         {
 
-            //Exercise 2: Print "Hello World" if x is equal to y.
+            //Exercise 3: Print "Yes" if x is equal to y, otherwise print "No".
+
 
             int x = 50;
             int y = 50;
             if(x == y) 
+{
+                Console.WriteLine("Yes");
+            }
+            else
             {
-                Console.WriteLine("Hello World");
+                Console.WriteLine("No");
             }
 
         }
