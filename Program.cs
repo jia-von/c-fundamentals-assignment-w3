bb5ad3c93aa25d7acb81ff894d1c829022e98f45
Jia <jiavon@ualberta.ca>
2020-09-01 10:11:08 -0600

    iniital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Fundamentals_Assignment_W3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial Commit");
+        }
+    }
+}
