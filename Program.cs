168a5f3fc3c518ad38b882cdb2ad80153c836527
Jia <jiavon@ualberta.ca>
2020-09-02 17:44:00 -0600

    comparing markscheme with answer worked well

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ namespace C_Fundamentals_Assignment_W3
     {
         static void Main(string[] args)
         {
-            
+            Testpaper paper1 = new Testpaper("Maths", new string[] { "1A", "2C", "3D", "4A", "5A" }, "60%");
+
+
+            Student student1 = new Student();
+            student1.TakeTest(paper1, new string[] { "1A", "2D", "3D", "4A", "5A" });
         }
 
 	}
--- Student.cs
+++ Student.cs
@@ -7,19 +7,55 @@ namespace C_Fundamentals_Assignment_W3
     public class Student : IStudent
     {
         //Property Implementation
-        public string[] TestsTaken { get; }
-        public string[] Answers { get; set; }
-        public ITestpaper Paper { get; set; }
+        private string[] _testsTaken;
+        public string[] TestsTaken {
+            get
+            { 
+                return _testsTaken;
+            } 
+            protected set
+            {
+                if (value.Length == 0)
+                {
+                    _testsTaken = new string[] { "No tests taken" };
+                }
+                else
+                {
+                    _testsTaken = value;
+                }
+            }
+        }
+        //public string[] Answers { get; set; }
+        //public ITestpaper Paper { get; set; }
+
+        //public void TakeTest(ITestpaper paper, string[] answers)
+        //{
+        //    Paper = paper;
+        //    Answers = answers;
+        //}
 
         public void TakeTest(ITestpaper paper, string[] answers)
         {
-            Paper = paper;
-            Answers = answers;
-        }
+            int matches = 0; // calculate the number of matches between answers and paper.MarkScheme
 
+            for (int i = 0; i<paper.MarkScheme.Length; i++)//start of comparison of the answer
+            {
+                bool temp = Array.Equals(answers[i], paper.MarkScheme[i]);
+
+                if (temp == true)
+                {
+                    matches++;
+                }
+            }//end of the comparison of answers
+            
+            //Calculate the percentage of the paper
+            int resultPercentage = 
+
+            //TestsTaken = new string[] {paper.Subject};
+        }
         public Student()
         {
-            TestsTaken = new string[] { };
+            TestsTaken = new string[]{};
         }
     }
 
