9fcdc4ff3ee1e1f7e94135a7770f676ff497ab28
Jia <jiavon@ualberta.ca>
2020-09-02 19:41:36 -0600

    completed and start a new one

--- Program.cs
+++ Program.cs
@@ -7,24 +7,7 @@ namespace C_Fundamentals_Assignment_W3
     {
         static void Main(string[] args)
         {
-            Testpaper paper1 = new Testpaper("Maths", new string[] { "1A", "2C", "3D", "4A", "5A" }, "60%");
-            Testpaper paper2 = new Testpaper("Chemistry", new string[] { "1C", "2C", "3D", "4A" }, "75%");
-            Testpaper paper3 = new Testpaper("Computing", new string[] { "1D", "2C", "3C", "4B", "5D", "6C", "7A" }, "75%");
 
-
-
-            Student student1 = new Student();
-            student1.TakeTest(paper1, new string[] { "1A", "2D", "3D", "4A", "5A" });
-            Console.WriteLine($"Student 1 Result: {student1.TestsTaken[0]}");
-
-            Student student2 = new Student();
-            student2.TakeTest(paper2, new string[] { "1C", "2D", "3A", "4C" });
-            student2.TakeTest(paper3, new string[] { "1A", "2C", "3A", "4C", "5D", "6C", "7B" });
-            Console.WriteLine($"Student 2 Results: {student2.TestsTaken[0]}");
-            Console.WriteLine($"Student 2 Results: {student2.TestsTaken[1]}");
-
-            Student student3 = new Student();
-            Console.WriteLine($"Student 3 Results: {student3.TestsTaken[0]}");
         }
 
 	}
